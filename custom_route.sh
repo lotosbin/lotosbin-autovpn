9dcc4ffbce90cda25785daaf8eca2794782f7e7e
lotosbin@gmail.com <lotosbin@gmail.com@a03530b6-b47e-978d-ca1a-0d9bc0f2e058>
2014-08-06 04:16:16 +0000

    .

--- custom_route.sh
+++ custom_route.sh
@@ -5,9 +5,10 @@ route add -net 173.255.114.0/24 dev $1 #angularjs.org
 route add -net 216.34.181.0/24 dev $1 #sourceforge.net
 
 #youtube
-route add -net 74.125.21.0/24 dev $1 #youtube.com
-route add -net 203.208.46.0/24 dev $1 #www.youtube.com
-
+##Name:      youtube.com
+#route add -net 216.34.181.0/24 dev $1 #Address 1: 2607:f8b0:4002:c06::88 yv-in-x88.1e100.net
+route add -net 74.125.21.0/24 dev $1 #Address 2: 74.125.21.91 yv-in-f91.1e100.net
+route add -net 203.208.46.0/24 dev $1 #www.youtube.com,accounts.youtube.com
 
 #github.com
 route add -net 207.97.227.0/24 dev $1 #github.com
