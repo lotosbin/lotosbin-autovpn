2468b8de621e6f5861fba92e8f5b2ca629beb856
lotosbin@gmail.com <lotosbin@gmail.com@a03530b6-b47e-978d-ca1a-0d9bc0f2e058>
2014-08-06 03:41:03 +0000

    .

--- trunk/custom_route.sh
+++ trunk/custom_route.sh
@@ -1,3 +1,9 @@
+#angularjs
 route add -net 173.255.114.0/24 dev $1 #angularjs.org
+
+#sourceforge
 route add -net 216.34.181.0/24 dev $1 #sourceforge.net
-route add -net 59.24.3.0/24 dev $1 #youtube.com
+
+#youtube
+route add -net 74.125.21.0/24 dev $1 #youtube.com
+route add -net 203.208.46.0/24 dev $1 #www.youtube.com
