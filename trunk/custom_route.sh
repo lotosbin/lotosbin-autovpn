1077a53458e1a8a509a9b36973da028eb8b722d3
lotosbin@gmail.com <lotosbin@gmail.com@a03530b6-b47e-978d-ca1a-0d9bc0f2e058>
2014-08-25 03:22:08 +0000

    s

--- trunk/custom_route.sh
+++ trunk/custom_route.sh
@@ -11,7 +11,7 @@ route add -net 74.125.21.0/24 dev $1 #Address 2: 74.125.21.91 yv-in-f91.1e100.ne
 route add -net 203.208.46.0/24 dev $1 #www.youtube.com,accounts.youtube.com
 
 #github.com
-route add -net 207.97.227.0/24 dev $1 #github.com
+#route add -net 207.97.227.0/24 dev $1 #github.com
 
 #amazon
 route add -net 72.52.4.0/24 dev $1 #s3.amazonaws.com
